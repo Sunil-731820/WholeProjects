d37962f36affdfb520144918ce395e3b691d8279
Sunil-731820 <sunilkumargupta1170@gmail.com>
2022-09-05 22:31:15 +0530

    "coded"

--- JAVAScriptProjects/StringMethods.js
+++ JAVAScriptProjects/StringMethods.js
@@ -19,3 +19,59 @@ let str1 = "Apple, Banana, Kiwi";
 let parts = str1.slice(-12,-6);
 console.log("the sclicing parts is "+ parts);
 
+// The use of the substring(start , end)
+
+let str12 = "Apple, Banana, Kiwi";
+console.log("The String is "+ str12);
+let parts1 = str12.substring(7,13);
+console.log("The SubString is parts2 is  " + parts1);
+
+console.log("again the subString is "+ str12.substr(-4));
+
+
+// The Use of the Replace()
+
+let str3 = "Please  visit to Microsoft";
+console.log("the string is "+ str3);
+let newText = str3.replace("Microsoft","Google");
+console.log("the new String is "+ newText);
+
+// The use /Microsoft/g i.e it will global override Or replace
+
+let str4 = "Please visit Microsoft and Microsoft";
+console.log("the String is "+ str4);
+let newtext1 = str4.replace(/Microsoft/g,"GeeksforGeeks");
+console.log("the new String is "+ newtext1);
+
+// The use of the toUpper()
+
+let text11 = "hello world";
+console.log("the string is "+ text11);
+let upperText = text11.toUpperCase();
+console.log("the Upper case is "+ upperText);
+
+// The use of the lowerCase()
+
+let text21 = "HELLO LINUS";
+console.log("the text is "+ text21);
+let lowerText = text21.toLowerCase();
+console.log("the string is " + lowerText);
+
+// The Use of the concate()
+let text41 = "Hello";
+let text42 = "Sunil";
+let text43 = text41.concat(" " ,text42);
+console.log("the text4 is "+ text43);
+
+// The Use of the trim()
+
+let text33 = "           Hello Sunil";
+console.log("the String is " + text33);
+let text44 = text33.trim();
+console.log("after the use of the trim()" + text44);
+
+// The use of the trimStart()
+let text51 = "          Hello World!              ";
+console.log("the string is "+ text51);
+let trimString = text51.trimStart();
+console.log("the String is "+ trimString);
